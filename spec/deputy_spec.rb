95278e0d8f94422e317c4010e5b549b869a22fef
thomasfrl <feral.thomas@gmail.com>
2019-01-17 18:25:17 +0100

    test 3 terminé

--- spec/deputy_spec.rb
+++ spec/deputy_spec.rb
@@ -1,12 +1,12 @@
-require_relative '../lib/townhall.rb'
-describe "cherche les urls des pages des villes" do
-  it "trouver l'url des 185 villes du Val d'Oise" do
-    expect(get_townhall_urls[1].size).to eq(185)
+require_relative '../lib/deputy.rb'
+describe "cherche les urls des pages des députés" do
+  it "trouver l'url des 577 députés" do
+    expect(get_deputy_urls[1].size).to eq(577)
   end
 end
 
-describe "cherche les emails des villes" do
-  it "trouver l'email d'une ville du Val d'Oise" do
-    expect(get_townhall_email("http://annuaire-des-mairies.com/95/arnouville-les-gonesse.html")).to eq("webmaster@villedarnouville.com")
+describe "cherche les emails des députés" do
+  it "trouver l'email d'un député" do
+    expect(get_deputy_email("https://www.nosdeputes.fr/berangere-abba")).to eq("berangere.abba@assemblee-nationale.fr")
   end
 end
